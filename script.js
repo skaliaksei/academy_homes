7b18b4d64dd3fd8419dc5886e6c6e3ec04f86af8
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-02 23:03:47 +0300

    feat: task 2 start

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var innerNum = prompt('Введите число');
-var addNum = prompt('Сколько прибавить?');
-var minusNum = prompt('Сколько отнять?');
-var multNum = prompt('На сколько умножить?');
-var divNum = prompt('На сколько разделить?');
+//Task 1
+// var innerNum = prompt('Введите число');
+// var addNum = prompt('Сколько прибавить?');
+// var minusNum = prompt('Сколько отнять?');
+// var multNum = prompt('На сколько умножить?');
+// var divNum = prompt('На сколько разделить?');
 
-var outerNum = (Number(innerNum) + Number(addNum) - Number(minusNum)) * Number(multNum) / Number(divNum);
+// var outerNum = (Number(innerNum) + Number(addNum) - Number(minusNum)) * Number(multNum) / Number(divNum);
 
-alert('Формула: (' + innerNum + ' + ' + addNum + ' - ' + minusNum + ') * ' + multNum + ' / ' + divNum + '\n' + 'Результат: ' + outerNum)
+// alert('Формула: (' + innerNum + ' + ' + addNum + ' - ' + minusNum + ') * ' + multNum + ' / ' + divNum + '\n' + 'Результат: ' + outerNum)
+
+//Task 2
+var entryData = prompt('Имя пользователя:', 'Админ');
+var password;
+
+if (!entryData) {
+    alert('Вход отменен');
+} else if (entryData === 'Админ') {
+    password = prompt('Пароль:', '123');
+} else {
+    alert('Я вас не знаю');
+}
