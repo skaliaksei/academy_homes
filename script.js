6199a951ea7d1ec733130372bb9167eb88e65d37
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-05 15:38:16 +0300

    feat: task_3

--- script.js
+++ script.js
@@ -10,20 +10,29 @@
 // alert('Формула: (' + innerNum + ' + ' + addNum + ' - ' + minusNum + ') * ' + multNum + ' / ' + divNum + '\n' + 'Результат: ' + outerNum)
 
 //Task 2
-var entryData = prompt('Имя пользователя:', 'Админ');
-var password;
+// var entryData = prompt('Имя пользователя:', 'Админ');
+// var password;
 
-if (!entryData) {
-    alert('Вход отменен');
-} else if (entryData === 'Админ') {
-    password = prompt('Пароль:', 'Чёрный Властелин');
-    if(!password) {
-        alert('Вход отменен');
-    } else if(password === 'Чёрный Властелин') {
-        alert('Добро пожаловать!');
-    } else {
-        alert('Пароль неверен');
-    }
-} else {
-    alert('Я вас не знаю');
-}
+// if (!entryData) {
+//     alert('Вход отменен');
+// } else if (entryData === 'Админ') {
+//     password = prompt('Пароль:', 'Чёрный Властелин');
+//     if(!password) {
+//         alert('Вход отменен');
+//     } else if(password === 'Чёрный Властелин') {
+//         alert('Добро пожаловать!');
+//     } else {
+//         alert('Пароль неверен');
+//     }
+// } else {
+//     alert('Я вас не знаю');
+// }
+
+//Task 3
+var message,
+    login;
+
+(login === 'Вася') ? message = 'Привет' :
+    (login === 'Директор') ? message = 'Здравствуйте' :
+        (login === '') ? message = 'Нет логина' :
+message = '';
