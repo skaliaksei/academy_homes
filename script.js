fbcd02a9026fb122bdccd4df56702146df958f12
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-06 19:47:02 +0300

    fix Task_3

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 //Task 1
-var innerNum = prompt('Введите число');
-var addNum = prompt('Сколько прибавить?');
-var minusNum = prompt('Сколько отнять?');
-var multNum = prompt('На сколько умножить?');
-var divNum = prompt('На сколько разделить?');
+// var innerNum = prompt('Введите число');
+// var addNum = prompt('Сколько прибавить?');
+// var minusNum = prompt('Сколько отнять?');
+// var multNum = prompt('На сколько умножить?');
+// var divNum = prompt('На сколько разделить?');
 
-var outerNum = (Number(innerNum) + Number(addNum) - Number(minusNum)) * Number(multNum) / Number(divNum);
+// var outerNum = (Number(innerNum) + Number(addNum) - Number(minusNum)) * Number(multNum) / Number(divNum);
 
-alert('Формула: (' + innerNum + ' + ' + addNum + ' - ' + minusNum + ') * ' + multNum + ' / ' + divNum + '\n' + 'Результат: ' + outerNum)
+// alert('Формула: (' + innerNum + ' + ' + addNum + ' - ' + minusNum + ') * ' + multNum + ' / ' + divNum + '\n' + 'Результат: ' + outerNum)
 
 //Task 2
-var entryData = prompt('Имя пользователя:', 'Админ');
-var password;
+// var entryData = prompt('Имя пользователя:', 'Админ');
+// var password;
 
-if (!entryData) {
-    alert('Вход отменен');
-} else if (entryData === 'Админ') {
-    password = prompt('Пароль:', 'Чёрный Властелин');
-    if(!password) {
-        alert('Вход отменен');
-    } else if(password === 'Чёрный Властелин') {
-        alert('Добро пожаловать!');
-    } else {
-        alert('Пароль неверен');
-    }
-} else {
-    alert('Я вас не знаю');
-}
+// if (!entryData) {
+//     alert('Вход отменен');
+// } else if (entryData === 'Админ') {
+//     password = prompt('Пароль:', 'Чёрный Властелин');
+//     if(!password) {
+//         alert('Вход отменен');
+//     } else if(password === 'Чёрный Властелин') {
+//         alert('Добро пожаловать!');
+//     } else {
+//         alert('Пароль неверен');
+//     }
+// } else {
+//     alert('Я вас не знаю');
+// }
 
 //Task 3
 var message,
-    login;
+    login = '';
 
-(login === 'Вася') ? message = 'Привет' :
-    (login === 'Директор') ? message = 'Здравствуйте' :
-        (login === '') ? message = 'Нет логина' :
-message = '';
+message = (login === 'Вася') ? 'Привет' :
+    (login === 'Директор') ? 'Здравствуйте' :
+        (login === '') ? 'Нет логина' : '';
+
 
 //Task 4
 var name,
