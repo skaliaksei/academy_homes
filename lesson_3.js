a8a4665d2977fc046a39bf2233faee687762582d
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-07 22:56:27 +0300

    Lesson_3 Task_2

--- lesson_3.js
+++ lesson_3.js
@@ -5,3 +5,13 @@ styles.push('Рок-н-Ролл');
 styles[styles.length - 2] = 'Классика';
 alert(styles.shift());
 styles.unshift('Рэп', 'Регги');
+
+// Task 2
+for (var i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+
+for (var i = 2; i <= 10; i++) {
+    if (i % 2 != 0) continue;
+    console.log(i);
+}
