0283a1f76346fd3e3c207a712c4970affb6819e9
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-26 21:45:08 +0300

    Task_3

--- lesson_5.js
+++ lesson_5.js
@@ -42,3 +42,32 @@ function sumTo(n) {
 }
 
 console.log(sumTo(9));
+
+// Task 3
+var arrTest = [1, 2, [3, 4, [4]]];
+var arr = [1, 2, 3, [4, 5, 6], [7, 8], 9, [10, [11, 12], 13]];
+var arrHW = [
+    5, 7,
+    [4, [2], 8, [1, 3], 2],
+    [9, [0]],
+    1, 8
+];
+
+
+
+function treeSum(arr) {
+    var result = 0;
+
+    for (key in arr) {
+        if (arr[key].length) {
+            result += treeSum(arr[key]);
+        } else {
+            if (!!arr[key]) {
+                result += arr[key];
+            }
+        }
+    }
+    return result;
+}
+
+console.log(treeSum(arrHW));
