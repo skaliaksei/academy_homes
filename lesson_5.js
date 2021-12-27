2c3f9e43e1845c09ac3a8fb7106cc39888327213
Aliaksei Skakun <skaliaksei@gmail.com>
2021-12-27 19:42:17 +0300

    fix: Task_3 checking by Number

--- lesson_5.js
+++ lesson_5.js
@@ -44,31 +44,21 @@ function sumTo(n) {
 console.log(sumTo(9));
 
 // Task 3
-var arrTest = [1, 2, [3, 4, [4]]];
-var arr = [1, 2, 3, [4, 5, 6], [7, 8], 9, [10, [11, 12], 13]];
 var arrHW = [
     5, 7,
     [4, [2], 8, [1, 3], 2],
-    [9, [0]],
+    [9, []],
     1, 8
 ];
-var x = [1, 2, 3, [], [3]];
-
-for (key in x) {
-    console.log(key + ' : ' + !!x[key] + ' : ' + x[key].length);
-}
-
-
-
 
 function treeSum(arr) {
     var result = 0;
 
     for (key in arr) {
         if (arr[key]) {
-            if (arr[key].length && arr[key].length > 0) {
+            if (arr[key].length && typeof(arr[key]) != 'string') { //Значение иимеет длину и это не строка
                 result += treeSum(arr[key]);
-            } else if (arr[key] > 0) {
+            } else if (typeof(arr[key]) == 'number') { //Значение- числовое
                 result += arr[key];
             }
         }
