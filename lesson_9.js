f136e42f72df2d0d06687bcaf299b5ccb175b318
Aliaksei Skakun <skaliaksei@gmail.com>
2022-01-16 14:30:53 +0300

    fix: remove Cat

--- lesson_9.js
+++ lesson_9.js
@@ -1,33 +1,4 @@
-// Task 3
-function Cat(name) {
-    this.name = name;
-    var foodAmount = 50;
-
-    this.feed = function() {
-        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
-    }
-
-    function formatFoodAmount() {
-        return foodAmount + ' гр.';
-    }
-
-    this.dailyNorm = function(amount) {
-        //Get:
-        if(!arguments.length) return formatFoodAmount();
-
-        //Set:
-        if (amount < 50) {
-            throw new Error('More than 50, please');
-        }
-        if (amount > 100) {
-            throw new Error('Less than 100, please');
-        }
-
-        foodAmount = amount;
-    }
-}
-
-// Task_4, Task_5
+// Task_3, Task_4, Task_5
 function Animal(name) {
     var self = this;
     self._name = name;
