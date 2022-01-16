cdd4c2ebaeaa14b7bd72f2969c0680e308e7a92b
Aliaksei Skakun <skaliaksei@gmail.com>
2022-01-16 20:48:34 +0300

    init: lesson_10.js

---
+++ lesson_10.js
@@ -0,0 +1,49 @@
+// Task_3, Task_4, Task_5
+function Animal(name) {
+    var self = this;
+    self._name = name;
+    var foodAmount = 50;
+
+    self.feed = function() {
+        console.log('Насыпаем в миску ' + self.dailyNorm() + ' корма.');
+    }
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    self.dailyNorm = function(amount) {
+        //Get:
+        if(!arguments.length) return formatFoodAmount();
+
+        //Set:
+        if (amount < 50) {
+            throw new Error('Значение не должно быть меньше 50');
+        }
+        if (amount > 100) {
+            throw new Error('Значение не должно быть больше 100');
+        }
+
+        foodAmount = amount;
+    }
+}
+
+function Cat() {
+    Animal.apply(this, arguments);
+
+    var animalFeed = this.feed;
+    this.feed = function() {
+        animalFeed();
+        console.log('Кот доволен ^_^');
+        return this;
+    }
+
+    this.stroke = function() {
+        console.log('Гладим кота.');
+        return this;
+    }
+}
+
+var barsik = new Cat('Barsik');
+barsik.dailyNorm(61);
+barsik.feed().stroke();
