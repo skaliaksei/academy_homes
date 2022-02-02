e19b48d9a4287b4a303afc5bc76fb4a174b74c8f
Aliaksei Skakun <skaliaksei@gmail.com>
2022-02-02 21:29:26 +0300

    L12 T5

--- Lesson_12/lesson_12.js
+++ Lesson_12/lesson_12.js
@@ -62,47 +62,66 @@ function transformParagraph(inputText) {
 
 transformParagraph(STARTTEXT);
 
-// Task_5 (не доделал)
-var TEXT = "Lorem ipsum ut enim ad minima veniam, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem!"
+// Task_5
+var TEXT = "Lorem ipsum apple ut enim ad minima veniam, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem!"
 
 function findRepeats(inputText) {
+    // Разбиваем на массив слов
 	var arrOfWords = inputText.split(/[\!\.\?\' '\,]/).filter(function(item) {
         return item.length > 0;
     });
 	var objOfWords = [];
-<<<<<<< HEAD
 
 	for (var i = 0; i < arrOfWords.length; i++) {
 		arrOfWords[i] = arrOfWords[i].toLowerCase();
 	}
 
-	for (var i = 0; i < arrOfWords.length; i++) {
+    for (var i = 0; i < arrOfWords.length; i++) {
+        //Находим позицию в массиве объектов
+        var currentPosition = findValueInArray(objOfWords, String(arrOfWords[i]));
 
+        // Если такой уже есть в objOfWords, то увеличиваем повторение
+        if (currentPosition != null) {
+            objOfWords[currentPosition].repeats += 1;
+        // Иначе, добавляем новый объект
+        } else {
+            objOfWords.push({value: String(arrOfWords[i]), repeats: 1})
+        }
+    }
 
-=======
-	
-	for (var i = 0; i < arrOfWords.length; i++) {
-		arrOfWords[i] = arrOfWords[i].toLowerCase();
-	}
-	
-	for (var i = 0; i < arrOfWords.length; i++) {
-		
-		
->>>>>>> 6bf1351ca61bb7b124d02f77c8954c76a290dfce
-	    var currentWordCount = objOfWords[arrOfWords[i]];
-		var count = currentWordCount ? currentWordCount : 0;
-		objOfWords[arrOfWords[i]] = count + 1;
-	}
-<<<<<<< HEAD
+    var mostPopularWord = findMostPopular(objOfWords);
 
-	//arrOfWords = arrOfWords.sort();
+    console.log('Максимальное число повторений у слова "' + mostPopularWord.value + '" - ' + mostPopularWord.repeats);
+}
 
-=======
-	
-	//arrOfWords = arrOfWords.sort();
-	
->>>>>>> 6bf1351ca61bb7b124d02f77c8954c76a290dfce
-	console.log(objOfWords);
+// Функция для поиска дубликатов
+function findValueInArray(inputArray, inputValue) {
+    var position = null;
+
+    inputArray.forEach(function(item, i) {
+        if (item.value === inputValue)
+            position = i;
+    });
+
+    return position;
+}
+
+// Функция для поиска наиболее повторяемых слов в массиве объектов:
+function findMostPopular(inputArr) {
+    var mostPopularWord = {
+        value: inputArr[inputArr.length - 1].value,
+        repeats: inputArr[inputArr.length - 1].repeats
+    };
+
+    for (var i = (inputArr.length - 2); i >= 0; i--) {
+
+        if (Number(inputArr[i].repeats) >= mostPopularWord.repeats) {
+            mostPopularWord.value = inputArr[i].value;
+            mostPopularWord.repeats = inputArr[i].repeats;
+        }
+    }
+
+    return mostPopularWord;
 }
 
 findRepeats(TEXT);
